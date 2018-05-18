20215505679032d28cff0bed9eb1be676345ec1d
Humberto Navarro <39270597+humbertong94@users.noreply.github.com>
2018-05-18 14:05:06 -0500

    Add files via upload

--- sparkml/build.sbt
+++ sparkml/build.sbt
@@ -1,7 +1,13 @@
+//[raken@cdh03 humberto]$ vi build.sbt
 name := "SparkMLExample"
 version := "1.0"
-scalaVersion := "2.x.x"
+scalaVersion := "2.11.8"
 
 javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
 
 // ADD CLOUDERA REPO AND DEPENDENCIES FOR SPARK APPS
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-core" % "2.3.0.cloudera2" % "provided")
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-sql" % "2.3.0.cloudera2" % "provided")
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-streaming-flume" % "2.3.0.cloudera2" % "provided")
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-streaming" % "2.3.0.cloudera2" % "provided")
+libraryDependencies ++= Seq("org.apache.spark" %% "spark-mllib" % "2.3.0.cloudera2" % "provided")
--- sparkml/run.sh
+++ sparkml/run.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-
 export SPARK_DIST_CLASSPATH=$(hadoop classpath)
-
 SPARK_CLASS=com.cloudera.sdk.SparkMLExample
-SPARK_BUILD_DIR=target/scala-2.11
-SPARK_JAR=sparkmlexample_2.11-1.0.jar
-SPARK_BASE=input/
+SPARK_BUILD_DIR=/home/raken/humberto_ml/sparkml
+SPARK_JAR=sparkmlexample.jar
+SPARK_BASE=/home/raken/humberto_ml/sparkml/input/
 ITERS=100
-
-DEPLOY_MODE=xxx
-MASTER=xxx
-
-spark2-submit --deploy-mode ${DEPLOY_MODE} --master ${MASTER} --executor-memory 1G --num-executors 2 --executor-cores 1 --class ${SPARK_CLASS} ${SPARK_BUILD_DIR}/${SPARK_JAR} ${SPARK_BASE} ${ITERS}
+DEPLOY_MODE=client
+MASTER=yarn
+spark2-submit  --master yarn --executor-memory 1G --num-executors 2 --executor-cores 1 --class ${SPARK_CLASS} ${SPARK_BUILD_DIR}/${SPARK_JAR} ${SPARK_BASE} ${ITERS}
